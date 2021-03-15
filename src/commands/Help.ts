637059b3a1d2e6c60b756322b1e5af036ccb7334
penta <martin@claesson.co>
2021-03-15 15:02:37 +0100

    add some help

--- src/commands/Help.ts
+++ src/commands/Help.ts
@@ -7,6 +7,7 @@ export default cmd
 export async function getHelp(message: Discord.Message, args: string[]) {
     let msg: string = "<@" + message.author.id + ">\n";
     let handled: Map<string, Command> = new Map<string, Command>()
+    msg += "The bots status will reflect the door status (Online=open, DND=closed\n"
     msg += "The following commands are currently supported:\n";
     commands.forEach((command: Command) => {
         if (!handled.has(command.name) && command.name != cmd.name) {
