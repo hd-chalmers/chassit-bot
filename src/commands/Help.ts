6283d393a3172d3adb269cb27835280770400355
Shinz <alen.mukaca@gmail.com>
2022-01-12 00:20:00 +0100

    🐛 Adjusted htmlparser to replaceAll and fixed som text errors in help along with adjusting text for context menu commands

--- src/commands/Help.ts
+++ src/commands/Help.ts
@@ -13,24 +13,24 @@ export abstract class HelpCMD {
       interaction: CommandInteraction
   ): Promise<void> {
     const commands = MetadataStorage.instance.applicationCommands.map((cmd) => {
-      return { name: cmd.name, description: cmd.description };
+      return { name: cmd.name, description: cmd.description, type: cmd.type };
     });
 
     const pages: MessageEmbed[] = []
 
-    const summary = new MessageEmbed().setFooter({text: 'Page 1 of ' + (commands.length + 1)}).setTitle('Alla kommandon').setDescription('De mesta kommandon kan också skrivas med `' + process.env.COMMAND_PREFIX ?? "!" + '`')
+    const summary = new MessageEmbed().setFooter({text: 'Page 1 of ' + (commands.length + 1)}).setTitle('Alla kommandon').setDescription('De mesta kommandon kan också skrivas med  **' + (process.env.COMMAND_PREFIX ?? "!") + '**')
     commands.map((cmd) => {
-      summary.addField('/'+cmd.name, cmd.description)
+      summary.addField((cmd.type === "CHAT_INPUT" ? '/' : '') + cmd.name, (cmd.type === "CHAT_INPUT" ? cmd.description: 'Höger klicka ett meddelande eller en användare och kolla under "Apps" för att nå kommandot'))
     })
 
     pages.push(summary)
 
     pages.push(...commands.map((cmd, i) => {
       return new MessageEmbed()
-        .setFooter({ text: `Page ${i + 2} of ${commands.length + 1}` })
+        .setFooter({ text: `Sida ${i + 2} av ${commands.length + 1}` })
         .setTitle("**Slash kommando info**")
         .addField("Namn", cmd.name)
-        .addField("Beskrivning", cmd.description);
+        .addField("Beskrivning", (cmd.type === "CHAT_INPUT" ? cmd.description: 'Höger klicka ett meddelande eller en användare och kolla under "Apps" för att nå kommandot'));
     }));
 
     const pagination = new Pagination(interaction, pages, {ephemeral: all !== 'alla', type: "BUTTON"});
@@ -40,12 +40,12 @@ export abstract class HelpCMD {
   @SimpleCommand('help', {aliases: ['commands', 'all-commands', 'kommandon', 'hjälp']})
   async helpList(command: SimpleCommandMessage){
     const commands = MetadataStorage.instance.applicationCommands.map((cmd) => {
-      return { name: cmd.name, description: cmd.description };
+      return { name: cmd.name, description: cmd.description, type: cmd.type };
     });
 
-    const summary = new MessageEmbed().setTitle('Alla kommandon').setDescription('De mesta kommandon kan också skrivas med `' + process.env.COMMAND_PREFIX ?? "!" + '`')
+    const summary = new MessageEmbed().setTitle('Alla kommandon').setDescription('De mesta kommandon kan också skrivas med **' + (process.env.COMMAND_PREFIX ?? "!") + '**')
     commands.map((cmd) => {
-      summary.addField('/'+cmd.name, cmd.description)
+      summary.addField((cmd.type === "CHAT_INPUT" ? '/' : '') + cmd.name, (cmd.type === "CHAT_INPUT" ? cmd.description: 'Höger klicka ett meddelande eller en användare och kolla under "Apps" för att nå kommandot'))
     })
     await command.message.reply({embeds: [summary]})
   }
